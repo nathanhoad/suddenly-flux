6e18d9c182380d4a83d0ab2a78110c0ae3d77971
Nathan Hoad <nathan@nathanhoad.net>
2019-08-20 09:40:05 +1000

    Exporting types

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,14 @@
 export { createState, createReducer, combineReducers } from './reducers';
 export { Store, Provider, connect } from './context';
+
+export {
+  ChangeListener,
+  State,
+  Action,
+  EventualAction,
+  Dispatch,
+  Reducer,
+  Reducers,
+  MapStateToPropsFunction,
+  MapDispatchToPropsFunction
+} from './types';
