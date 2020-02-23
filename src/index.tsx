d048f5e6c54554473d2909f3cf7751e63f159a97
Nathan Hoad <nathan@nathanhoad.net>
2020-02-23 19:29:58 +1000

    Fix missing hook exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 export { createState, createReducer, combineReducers } from "./reducers";
 export { Context, Store, Provider, connect } from "./context";
+export { useSelector, useDispatch } from "./hooks";
 export { queryString } from "./actions";
 
 export {
